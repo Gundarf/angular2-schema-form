cd7a67b1dc5d84b27cf87e31859ae8f551420f69
Julien MOUTEL <julien.moutel@isae-supaero.fr>
2016-12-07 08:32:09 +0100

    Add tab level buttons

--- src/schema-form/defaultwidgets/object/object.widget.ts
+++ src/schema-form/defaultwidgets/object/object.widget.ts
@@ -1,11 +1,64 @@
 import {
   Component,
+  OnInit
 } from "@angular/core";
 
 import { ObjectLayoutWidget } from "../../widget";
+
+import {
+  ActionRegistry
+} from "../../model";
 
 @Component({
   selector: "form-object",
   template: require("./object.widget.html")
 })
-export class ObjectWidget extends ObjectLayoutWidget { }
+export class ObjectWidget extends ObjectLayoutWidget implements OnInit {
+
+  private buttons = [];
+
+  constructor(private actionRegistry: ActionRegistry) {
+    super();
+  }
+
+  ngOnInit() {
+    this.parseButtons();
+  }
+
+  private parseButtons() {
+    if (this.formProperty.schema.tabs !== undefined) {
+      for (let tab of this.formProperty.schema.tabs) {
+        if (tab.buttons !== undefined) {
+          this.buttons = this.buttons.concat(tab.buttons);
+        }
+      }
+      for (let button of this.buttons) {
+        this.createButtonCallback(button);
+      }
+    }
+  }
+
+  private createButtonCallback(button) {
+    button.action = (e) => {
+      let action;
+      if (button.id && (action = this.actionRegistry.get(button.id))) {
+        if (action) {
+          action(this.formProperty, button.parameters);
+        }
+      }
+      e.preventDefault();
+    };
+  }
+
+  getButtons(tab) {
+    let result = [];
+    for (let button of this.buttons) {
+      for (let tabBtn of tab.buttons) {
+        if (button.id == tabBtn.id) {
+          result.push(button);
+        }
+      }
+    }
+    return result;
+  }
+}
