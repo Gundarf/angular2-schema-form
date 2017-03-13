124f85f468251fffe52e23522093e2de9cc708c2
Julien MOUTEL <julien.moutel@isae-supaero.fr>
2017-03-13 12:48:28 +0100

    Conditional tabs depending on the value of a property

--- src/schema-form/defaultwidgets/object/object.widget.ts
+++ src/schema-form/defaultwidgets/object/object.widget.ts
@@ -2,8 +2,10 @@ import {
   Component,
   OnInit
 } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
 
 import { ObjectLayoutWidget } from '../../widget';
+import { FormProperty, PropertyGroup } from '../../model/formproperty';
 
 import {
   ActionRegistry
@@ -23,6 +25,62 @@ export class ObjectWidget extends ObjectLayoutWidget implements OnInit {
 
   ngOnInit() {
     this.parseButtons();
+    this.bindTabsVisibility();
+  }
+
+  private bindTabsVisibility() {
+    if (this.formProperty.schema.tabs !== undefined) {
+      this.formProperty.schema.tabs.forEach(tab => {
+        this.bindTabVisibility(tab);
+      });
+    }
+  }
+
+  protected searchProperty(path: string, proper: FormProperty): FormProperty {
+    let prop: FormProperty = proper;
+    let base: PropertyGroup = null;
+    let result = null;
+    base = proper.findRoot();
+    if (path[0] === '/') {
+      result = base.getProperty(path.substr(1));
+    } else {
+      result = base.getProperty(path);
+    }
+    return result;
+  }
+
+  private bindTabVisibility(tab) {
+    let visibleIf = tab.visibleIf;
+    if (visibleIf !== undefined) {
+      let propertiesBinding = [];
+      for (let dependencyPath in visibleIf) {
+        if (visibleIf.hasOwnProperty(dependencyPath)) {
+          let property = this.searchProperty(dependencyPath, this.formProperty);
+          if (property) {
+            let valueCheck = property.valueChanges.map(
+              value => {
+                if (visibleIf[dependencyPath].indexOf('$ANY$') !== -1) {
+                  return value.length > 0;
+                }else {
+                  return visibleIf[dependencyPath].indexOf(value) !== -1;
+                }
+              }
+            );
+        //    let visibilityCheck = property._visibilityChanges;
+          //  let and = Observable.combineLatest([valueCheck, visibilityCheck], (v1, v2) => v1 && v2);
+            propertiesBinding.push(valueCheck);
+          } else {
+            console.warn('Can\'t find property ' + dependencyPath + ' for visibility check of ' + tab.id);
+          }
+        }
+      }
+
+      Observable.combineLatest(propertiesBinding, (...values: boolean[]) => {
+        return values.indexOf(true) !== -1;
+      }).distinctUntilChanged().subscribe((visible) => {
+        tab.visible = visible;
+      });
+    }
   }
 
   private parseButtons() {
@@ -97,5 +155,25 @@ export class ObjectWidget extends ObjectLayoutWidget implements OnInit {
       classes = 'btn ' + classes;
     }
     return classes;
+  }
+
+  private isTabVisible(tab: any): boolean {
+    let retour: boolean = true;
+    if (tab !== undefined && tab.visible != undefined) {
+      retour = tab.visible
+    }
+    return retour;
+  }
+
+  getTabList() {
+    let retour:any[] = [];
+      if (this.formProperty.schema.tabs !== undefined) {
+      this.formProperty.schema.tabs.forEach(tab => {
+        if (this.isTabVisible(tab)) {
+          retour.push(tab);
+        }
+      });
+    }
+    return retour;
   }
 }
