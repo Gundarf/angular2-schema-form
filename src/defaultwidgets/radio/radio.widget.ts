0318c621960a4c05020be7b6fb11c8fea11d6d4d
Julien MOUTEL <julien.moutel@isae-supaero.fr>
2017-04-19 10:51:50 +0200

    changes for input/container class control

--- src/defaultwidgets/radio/radio.widget.ts
+++ src/defaultwidgets/radio/radio.widget.ts
@@ -7,23 +7,25 @@ import { ControlWidget } from '../../widget';
   selector: 'sf-radio-widget',
   template: `<div class="widget form-group">
   	<div [class]="formProperty.schema.htmlClass" [formGroup]=fg>
-  	<label>{{schema.title}}</label>
-  	<div *ngFor="let option of schema.oneOf" class="radio-inline">
-  		<input [formControl]="control" [attr.name]="name" formControlName="{{name}}" value="{{option.enum[0]}}" type="radio"  [attr.disabled]="schema.readOnly">
-  		<label class="horizontal control-label">
-  			{{option.description}}
-  		</label>
-  	</div>
-  	<input *ngIf="schema.readOnly" [attr.name]="name" type="hidden" [formControl]="control">
-  	<span *ngIf="schema.description" class="formHelp">{{schema.description}}</span>
+    	<label>{{schema.title}}</label>
+    	<div *ngFor="let option of schema.oneOf" [class]="inputClass">
+    		<input [formControl]="control" [attr.name]="name" formControlName="{{name}}" value="{{option.enum[0]}}" type="radio"  [attr.disabled]="schema.readOnly">
+    		<label class="horizontal control-label">
+    			{{option.description}}
+    		</label>
+    	</div>
+    	<input *ngIf="schema.readOnly" [attr.name]="name" type="hidden" [formControl]="control">
+    	<span *ngIf="schema.description" class="formHelp">{{schema.description}}</span>
   	</div>
   </div>`
 })
 export class RadioWidget extends ControlWidget implements OnInit{
   fg: FormGroup = new FormGroup({});
-
+  inputClass: string = ""
   ngOnInit() {
     this.fg.addControl(this.name, this.control);
+    if (this.formProperty.schema.inputClass) this.inputClass = this.formProperty.schema.inputClass;
+    else this.inputClass="radio-inline"
   }
 
 }
--- src/defaultwidgets/textarea/textarea.widget.ts
+++ src/defaultwidgets/textarea/textarea.widget.ts
@@ -1,15 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { ControlWidget } from '../../widget';
 
 @Component({
   selector: 'sf-textarea-widget',
   template: `<div class="widget form-group">
+  <div [class]="formProperty.schema.htmlClass">
+
   	<label [attr.for]="id" class="horizontal control-label">
   		{{ schema.title }}
   	</label>
       <span *ngIf="schema.description" class="formHelp">{{schema.description}}</span>
-  	<textarea [tooltip]="schema.tooltip" [tooltipEnable]="!!schema.tooltip" [attr.readonly]="schema.readOnly" [name]="name" class="text-widget textarea-widget form-control" [formControl]="control"></textarea>
+  	<textarea [tooltip]="schema.tooltip" [tooltipEnable]="!!schema.tooltip" [attr.readonly]="schema.readOnly" [name]="name" [class]="inputClass" [formControl]="control"></textarea>
+    </div>
   </div>`
 })
-export class TextAreaWidget extends ControlWidget {}
+export class TextAreaWidget extends ControlWidget  implements OnInit {
+  inputClass: string=""
+
+  ngOnInit() {
+    this.inputClass="text-widget textarea-widget form-control"
+    if (this.formProperty.schema.inputClass)
+    this.inputClass = this.inputClass +" " + this.formProperty.schema.inputClass
+  }
+}
